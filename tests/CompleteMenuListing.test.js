56b837d73a14683f5a60442d5085f5fdd96ae4e7
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-20 16:56:54 -0700

    added dummy props to completeMenuListing test

--- tests/CompleteMenuListing.test.js
+++ tests/CompleteMenuListing.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CompleteMenuListing from '../client/components/CompleteMenuListing'
 
+const dummyProps = {
+  menu: {
+    sections: ['one', 'two', 'three']
+  }
+};
+
 it('mounts', () => {
-  const wrap = CompleteMenuListing();
+  const wrap = CompleteMenuListing(dummyProps);
   expect(wrap.exists());
 });
