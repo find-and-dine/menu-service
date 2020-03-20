7d3199fcce5112bb3f36780229f43ba5d3273e6e
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-20 13:47:51 -0700

    asdfasdfasdfasdfasdfasdfasdf

--- tests/Footer.test.js
+++ tests/Footer.test.js
@@ -6,22 +6,21 @@ import Footer from '../client/components/Footer'
 it('mounts', () => {
   const wrap = shallow(<Footer />
   );
-  expect(
-    // TODO: come back and debug matchingElement test
-    expect(wrap.containsMatchingElement(
-      <div className="previewFooter">
-        <img
-          src="../resources/menuIcon.png"
-          alt=""
-          height="18px"
-          width="auto"
-        />
-        <span className="previewFooterItem">
-          Show full menu
+
+  // TODO: come back and debug matchingElement test
+  expect(wrap.containsMatchingElement(
+    <div className="previewFooter">
+      <img
+        src="../resources/menuIcon.png"
+        alt=""
+        height="18px"
+        width="auto"
+      />
+      <span className="previewFooterItem">
+        Show full menu
           </span>
-      </div>
-      )
-    ).toBeTruthy()
+    </div>
   )
+  ).toBeTruthy()
 });
 
