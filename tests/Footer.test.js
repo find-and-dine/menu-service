1654ccd68b0e8c1c7ad587bedbd6ba4a21b2ba9a
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-20 13:45:49 -0700

    the struggle bus has uncomfortable seats

--- tests/Footer.test.js
+++ tests/Footer.test.js
@@ -2,28 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Footer from '../client/components/Footer'
 
-describe('Footer component', () => {
-  it('mounts', () => {
-    const wrap = shallow(
-      <Footer />
-    )
+
+it('mounts', () => {
+  const wrap = shallow(<Footer />
+  );
   expect(
-    wrap.find('span').text()
-  ).toEqual('Show full menu');
     // TODO: come back and debug matchingElement test
-    /* expect(wrap.containsMatchingElement(
-        <div className="previewFooter">
-          <img
-            src="../resources/menuIcon.png"
-            alt=""
-            height="18px"
-            width="auto"
-          />
-          <span className="previewFooterItem">
-            Show full menu
+    expect(wrap.containsMatchingElement(
+      <div className="previewFooter">
+        <img
+          src="../resources/menuIcon.png"
+          alt=""
+          height="18px"
+          width="auto"
+        />
+        <span className="previewFooterItem">
+          Show full menu
           </span>
-        </div>
+      </div>
       )
-    ).toBeTruthy() */
-  })
+    ).toBeTruthy()
+  )
 });
+
