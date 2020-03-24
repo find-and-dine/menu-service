1379b4c62468e60fdf1c77deed7381a8a62ab5e6
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-03-24 14:38:56 -0700

    installed style loader and css loader, now bunldes css styles directly

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import path from 'path';
 import App from './components/App';
+import './styles.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App id={path.basename(window.document.URL)} />, document.getElementById('app'));
